4d25a84370560e2316e6b542409d175e2f68e8c0
ravi8322 <110557813+ravi8322@users.noreply.github.com>
2022-08-09 22:29:25 +0530

    Add files via upload

---
+++ Area.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  9 22:11:27 2022
+
+@author: RAVI TEJ
+"""
+
+print("Area Of The Circle")
+r = float(input("Radius : "))
+a = 3.14159 * r * r
+print("Area : ", a)
+
+
