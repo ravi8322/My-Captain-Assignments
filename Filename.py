0ab4fd1769bd825966ce69c5a198a9a481b3845a
ravi8322 <ravitej1818@gmail.com>
2022-08-11 22:22:29 +0530

    latest update

---
+++ Filename.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  9 22:32:31 2022
+
+@author: RAVI TEJ
+"""
+
+print("File Name Extensions")
+fn= input("Filename : ")
+f= fn.split(".")
+print("Extension : " + f[-1])
+
